6201b74ea179dec585f1a5aae95cf5639f084dce
Christopher Mehmed <chrismehmed@gmail.com>
2014-08-12 21:19:00 -0400

    Start Branching

--- Above_Average/config/routes.rb
+++ Above_Average/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root to: "home#index"
+  root to: 'home#index'
+
+  get '/punch' => 'punch#index', as: :punch
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
