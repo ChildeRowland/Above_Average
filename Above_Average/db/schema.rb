3d97d6b2640853f2418b75d5195ef7c47a39ff3b
Christopher Mehmed <chrismehmed@gmail.com>
2014-08-21 21:20:52 -0400

    postgres database

--- Above_Average/db/schema.rb
+++ Above_Average/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140822003327) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "profiles", force: true do |t|
     t.integer  "age"
     t.integer  "zip"
@@ -33,15 +36,6 @@ ActiveRecord::Schema.define(version: 20140822003327) do
     t.datetime "updated_at"
   end
 
-  create_table "test_forms", force: true do |t|
-    t.integer  "stop_bank_robbers"
-    t.integer  "love_litter"
-    t.boolean  "inverse_statement"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -57,8 +51,8 @@ ActiveRecord::Schema.define(version: 20140822003327) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "utilities", force: true do |t|
     t.integer  "kwh"
