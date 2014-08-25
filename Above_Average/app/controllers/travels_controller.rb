40b7c2b6e168672f27a708df8299b5a8b8b417e3
Christopher Mehmed <chrismehmed@gmail.com>
2014-08-25 15:51:21 -0400

    multiple travel inputs

--- Above_Average/app/controllers/travels_controller.rb
+++ Above_Average/app/controllers/travels_controller.rb
@@ -5,6 +5,19 @@ class TravelsController < ApplicationController
 		@travel = @user.travels.build(params[:travel])
 	end
 
+
+	# number_string = params[:distance_comma_separated]
+
+	# walk_list = number_string.split(',')
+	# total = 0
+	# 	walk_list.each do |num|
+ #  			total += num
+	# 	end
+	
+	# walk = total
+	# walk.save
+
+
 	def create
 		@user = current_user
 		@travel = @user.travels.build(travel_params)
