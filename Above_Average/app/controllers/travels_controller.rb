18052d8c479e1795b261220cb53c5dfdd3e9be66
Christopher Mehmed <chrismehmed@gmail.com>
2014-08-26 17:10:59 -0400

    travel form submit

--- Above_Average/app/controllers/travels_controller.rb
+++ Above_Average/app/controllers/travels_controller.rb
@@ -5,19 +5,6 @@ class TravelsController < ApplicationController
 		@travel = @user.travels.build(params[:travel])
 	end
 
-
-	# number_string = params[:distance_comma_separated]
-
-	# walk_list = number_string.split(',')
-	# total = 0
-	# 	walk_list.each do |num|
- #  			total += num
-	# 	end
-	
-	# walk = total
-	# walk.save
-
-
 	def create
 		@user = current_user
 		@travel = @user.travels.build(travel_params)
@@ -34,3 +21,14 @@ class TravelsController < ApplicationController
 	end
 
 end
+
+# number_string = params[:distance_comma_separated]
+
+	# walk_list = number_string.split(',')
+	# total = 0
+	# 	walk_list.each do |num|
+ #  			total += num
+	# 	end
+	
+	# walk = total
+	# walk.save
