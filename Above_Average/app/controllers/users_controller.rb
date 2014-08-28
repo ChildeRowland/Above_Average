4ea36531deafc97757f40c698ca13fbfb69dd8f7
Christopher Mehmed <chrismehmed@gmail.com>
2014-08-28 00:07:55 -0400

    no form/graph redirect

--- Above_Average/app/controllers/users_controller.rb
+++ Above_Average/app/controllers/users_controller.rb
@@ -30,55 +30,56 @@ class UsersController < ApplicationController
 		# 	flash[:notice] = "Please complete the Travel survey to continue"
 		# 	redirect_to new_user_travel_path @current_user
 		# end
-	
-
-		# @profile = @user.profiles
-	 	@profiles = Profile.all
-		@profile = Profile.last(params[:profile])
 		
-		@utility = @user.utilities
-		@utilities = Utility.all
-		
-		@recycling = @user.recyclings
-		@recyclings = Recycling.all
 
-		@travel = @user.travels
-		@travels = Travel.all
-#profile
-		@average_age = Profile.average(:age)
-		@average_total_household_members = Profile.average(:total_household_members)
-		@average_zip = Profile.average(:zip)
-#utlity
-		@average_kwh = Utility.average(:kwh)
-		@average_therms = Utility.average(:therms)
-		@average_gallons = Utility.average(:gallons)
-#recycling
-		@average_local = Recycling.average(:local)
-		@average_compost = Recycling.average(:compost)
-		@average_fabric = Recycling.average(:fabric)
-		@average_ewaste = Recycling.average(:ewaste)
+			# @profile = @user.profiles
+		 	@profiles = Profile.all
+			@profile = Profile.last(params[:profile])
+			
+			@utility = @user.utilities
+			@utilities = Utility.all
+			
+			@recycling = @user.recyclings
+			@recyclings = Recycling.all
 
-# travel
-		@average_walk = Travel.average(:walk)
-		@average_bicycle = Travel.average(:bicycle)
-		@average_train = Travel.average(:train)
-		@average_bus = Travel.average(:bus)
-		@average_car = Travel.average(:car)
-		@average_plane = Travel.average(:plane)
-		
-# travel & utility totals
-		
-		@user_average_total_travels = Travel.where('user_id = ?', @user.id).average(:total)
-		@user_average_total_utilities = Utility.where('user_id = ?', @user.id).average(:total)
-		if !@user.travels.empty? && !@user.utilities.empty?
-			@CO2 = (@user_average_total_travels + @user_average_total_utilities) 
-		end
+			@travel = @user.travels
+			@travels = Travel.all
+	#profile
+			@average_age = Profile.average(:age)
+			@average_total_household_members = Profile.average(:total_household_members)
+			@average_zip = Profile.average(:zip)
+	#utlity
+			@average_kwh = Utility.average(:kwh)
+			@average_therms = Utility.average(:therms)
+			@average_gallons = Utility.average(:gallons)
+	#recycling
+			@average_local = Recycling.average(:local)
+			@average_compost = Recycling.average(:compost)
+			@average_fabric = Recycling.average(:fabric)
+			@average_ewaste = Recycling.average(:ewaste)
 
-		@average_total_travels = Travel.average(:total)
-		@average_total_utilities = Utility.average(:total)
-		if !@user.travels.empty? && !@user.utilities.empty?
-			@CO2 = [@average_total_travels + @average_total_utilities]
-		end
+	# travel
+			@average_walk = Travel.average(:walk)
+			@average_bicycle = Travel.average(:bicycle)
+			@average_train = Travel.average(:train)
+			@average_bus = Travel.average(:bus)
+			@average_car = Travel.average(:car)
+			@average_plane = Travel.average(:plane)
+			
+	# travel & utility totals
+			
+			@user_average_total_travels = Travel.where('user_id = ?', @user.id).average(:total)
+			@user_average_total_utilities = Utility.where('user_id = ?', @user.id).average(:total)
+			if !@user.travels.empty? && !@user.utilities.empty?
+				@CO2 = (@user_average_total_travels + @user_average_total_utilities) 
+			end
+
+			@average_total_travels = Travel.average(:total)
+			@average_total_utilities = Utility.average(:total)
+			if !@user.travels.empty? && !@user.utilities.empty?
+				@CO2 = [@average_total_travels + @average_total_utilities]
+			end
+
 
 	end
 
